8e90eeda05f2aa6a911978d1cc1cb224f200fbc5
Boldecca <muteterirebecca60@gmail.com>
2025-11-19 22:46:03 +0200

    updated next.config.ts file

--- medium-clone/next.config.ts
+++ medium-clone/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
+  },
 };
 
 export default nextConfig;
