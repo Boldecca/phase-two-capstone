1b30109ea389c75b790ee37d028be6b6e9b3fff3
Boldecca <muteterirebecca60@gmail.com>
2025-11-19 20:03:07 +0200

    updated signin page

--- medium-clone/src/app/signin/page.tsx
+++ medium-clone/src/app/signin/page.tsx
@@ -16,7 +16,7 @@ export default function SigninPage() {
 
         <div className="mt-6 text-center">
           <p className="text-muted-foreground">
-            Don't have an account?{' '}
+            Don&apos;t have an account?{' '}
             <Link href="/signup" className="font-semibold text-foreground hover:underline">
               Sign up
             </Link>
